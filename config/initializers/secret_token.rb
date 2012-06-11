78551e4acfbf22ec334de3967e1eb7077266b57e
Akash Chauhan <akash6190@gmail.com>
2012-06-11 14:33:43 +0000

    Initial Directory structure for Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '0b7298c5aa342ee7869138b871234894e09f7ca67e2f880462e597ee19ac3dbc8dda707897d7398dd503207f6e3b6c5c7957b19744fae4160af085281fcee13b'
